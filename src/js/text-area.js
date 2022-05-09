b38313c42a996493f373ddb0789df6ed1fbe0407
Alexander Tretyakov <tpetyakov@gmail.com>
2022-05-10 01:30:36 +0300

    feat: make arrowUp, arrowDown work

--- src/js/text-area.js
+++ src/js/text-area.js
@@ -5,6 +5,7 @@ class TextArea {
     this.class = 'textarea';
     this.el = this.render();
     this.setCursor(this.value.length);
+    this.el.addEventListener('click', this.handleClick);
   }
 
   get value() {
@@ -23,6 +24,10 @@ class TextArea {
   isFocused() {
     return document.activeElement === this.el;
   }
+
+  handleClick = () => {
+    this.isDecrease = false;
+  };
 
   setCursor(pos) {
     this.el.selectionStart = pos;
@@ -67,20 +72,86 @@ class TextArea {
     // '◀';
     const { selection: { start } } = this;
     this.setCursor(start === 0 ? start : start - 1);
+    this.isDecrease = false;
   }
 
   arrowRight() {
     // '▶';
     this.setCursor(this.selection.start + 1);
+    this.isDecrease = false;
+  }
+
+  _getEolsPositions() {
+    let sum = 0;
+    return this.el.value
+      .split('\n')
+      .map((row) => row.length)
+      .map((len, i) => {
+        const pos = len + i + sum;
+        sum += len;
+        return pos;
+      })
+      .slice(0, -1);
+  }
+
+  _getRowsPositions(rows) {
+    const eolsPositions = this._getEolsPositions();
+    let lastPosition = 0;
+    return rows.map((row, i) => {
+      const start = lastPosition;
+      let end = lastPosition + row.length - (i === (rows.length - 1) ? 0 : 1);
+      if (eolsPositions.find((v) => v === end + 1)) {
+        end += 1;
+        lastPosition += 1;
+      }
+      lastPosition += row.length;
+      return { start, end };
+    });
+  }
+
+  _findRowIndex() {
+    const { selection: { start: cursorPos } } = this;
+    const rows = new FormData(this.el.parentElement).get('text').split('\n');
+    const rowsPositions = this._getRowsPositions(rows);
+    const index = rowsPositions.findIndex(({ start, end }) => (
+      cursorPos >= start && cursorPos <= end
+    ));
+    return { index, rowsPositions, cursorPos };
+  }
+
+  _moveVertically(direction) {
+    const { index, rowsPositions, cursorPos } = this._findRowIndex();
+    if (!rowsPositions[index + direction]) {
+      return false;
+    }
+    const { start, end } = rowsPositions[index + direction];
+
+    let rowOffset = cursorPos - rowsPositions[index].start;
+    if (this.isDecrease && rowOffset < this.prevOffset) {
+      rowOffset = this.prevOffset;
+    }
+    this.isDecrease = rowOffset > end - start;
+    if (this.isDecrease) this.prevOffset = rowOffset;
+
+    this.setCursor(this.isDecrease ? end : start + rowOffset);
+    return true;
   }
 
   arrowUp() {
-    // TODO: FormData, canvas textmeasure
-    this.addText('▲');
+    // canvas textmeasure
+    // this.addText('▲');
+
+    if (!this._moveVertically(TextArea.MOVE_DIRECTION.UP)) {
+      this.setCursor(0);
+    }
   }
 
   arrowDown() {
-    this.addText('▼');
+    // this.addText('▼');
+
+    if (!this._moveVertically(TextArea.MOVE_DIRECTION.DOWN)) {
+      this.setCursor(this.el.value.length);
+    }
   }
 
   enter() {
@@ -125,10 +196,16 @@ class TextArea {
     el.setAttribute('cols', '78');
     el.setAttribute('rows', '10');
     el.setAttribute('wrap', 'hard');
+    el.setAttribute('name', 'text');
     el.textContent = 'Focus me';
     return el;
   }
 }
+
+TextArea.MOVE_DIRECTION = {
+  UP: -1,
+  DOWN: 1,
+};
 
 TextArea.SELECT_MODE = {
   RIGHT: 'right',
